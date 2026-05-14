a91485f87cfd1dc597a5b944b4f0dd37fa79df16
61m <2499576814@qq.com>
2026-05-14 14:25:17 +0800

    添加了一份Exp16准稳态法测不良导体的比热容和导热系数的MATLAB画图程序

--- Exp16/画图（25级 61m）/Exp16.m
+++ Exp16/画图（25级 61m）/Exp16.m
@@ -1,11 +1,13 @@
 clear; clc; close all;
 time = 0:25;
+%电动势与温度关系（μV/K）
+sensitivity = 40;
 %中心面热电势
 S_center = [36, 43, 56, 78, 104, 132, 160, 189, 218, 247, 275, 304, 331, 360, 387, 415, 442, 470, 497, 523, 550, 576, 603, 629, 655, 680];
-T_center = S_center ./ 40;
+T_center = S_center ./ sensitivity;
 %加热面热电势
 S_heat = [60, 143, 189, 226, 259, 289, 318, 346, 375, 404, 431, 459, 487, 514, 542, 569, 596, 623, 650, 677, 703, 729, 755, 781, 807, 823];
-T_heat = S_heat ./ 40;
+T_heat = S_heat ./ sensitivity;
 %△T
 T_delta = T_heat - T_center;
 %画图
